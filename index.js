00a25c13efabb5f591e37448e4d2a21dadaffa6e
Joe Dimartino <j.dimartino11@gmail.com>
2020-11-04 18:19:21 -0500

    Add login page and route on server

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const EXPRESS = require('express');
 const PATH = require('path');
+const ROUTER = EXPRESS.Router();
 const PORT = process.env.PORT || 3002;
 const bodyParser = require('body-parser');
 const {cloudinary} = require('./Uploader/something.js');
@@ -8,12 +9,12 @@ const axios = require('axios');
 
 const APP = EXPRESS();
 
-APP.use(EXPRESS.static(PATH.join(__dirname, './dist')));
+APP.use(EXPRESS.static(PATH.join(__dirname, 'dist')));
+const distPath = PATH.join(__dirname, 'dist');
 APP.use(bodyParser.json({limit:'50mb'}));
 APP.use(bodyParser.urlencoded({ extended: true }));
 
-
-APP.get('/', (req, res) => {
+ROUTER.get('/', (req, res) => {
   res.status(200).send('index');
 });
 
@@ -30,6 +31,10 @@ APP.post('/postImage', async (req, res) => {
   } catch (error) {
     res.sendStatus(404);
   }
+});
+
+APP.get('/login', (req, res) => {
+  res.sendFile(distPath + '/login.html');
 });
 
 APP.listen(PORT, () => {
