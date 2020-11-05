f1b68406bca8991f8b48630e1770fe086c321044
Joe Dimartino <j.dimartino11@gmail.com>
2020-11-05 18:38:48 -0500

    Add interaction tracker

---
+++ src/utility/InteractionTracker.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import axios from 'axios';
+
+class InteractionTracker extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.postInteraction = this.postInteraction.bind(this);
+  }
+
+  postInteraction() {
+    axios.post('https://nameless-mountain-18450.herokuapp.com/', {
+      action: this.props.action,
+      userLat: this.props.userLat,
+      userLng: this.props.userLng
+    })
+    .then((response) => {
+      console.log('success posting interaction', response);
+    })
+    .catch((error) => {
+      console.log('error posting interaction', error);
+    })
+  }
+
+  render() {
+    return this.props.render({
+      postInteraction: this.postInteraction
+   })
+  }
+}
+
+export default InteractionTracker;
